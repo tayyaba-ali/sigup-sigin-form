311b893d2cb7e9258e802bd0ce1bd97fbecc8860
Umra-Badar20 <umrabadar555@gmail.com>
2024-09-25 10:44:02 +0500

    done

--- app.js
+++ app.js
@@ -1,45 +1,86 @@
 
-var usersData = []
+// var usersData = []
 
-var registerBtn = document.getElementById("register");
-registerBtn && registerBtn.addEventListener("click",function(){
+// var registerBtn = document.getElementById("register");
+// registerBtn && registerBtn.addEventListener("click", function () {
 
-    var rName = document.getElementById("registerName")
-    var rEmail = document.getElementById("registerEmail")
-    var rPassword = document.getElementById("registerPass")
-
-
-
-    var userObj = {
-        name:rName.value,
-        email:rEmail.value,
-        password:rPassword.value
-    }
-    usersData.push(userObj)
+//     var rName = document.getElementById("registerName")
+//     var rEmail = document.getElementById("registerEmail")
+//     var rPassword = document.getElementById("registerPass")
 
 
-    rName.value = ""
-    rEmail.value = ""
-    rPassword.value = ""
 
-    localStorage.setItem("users",JSON.stringify(usersData))
+//     var userObj = {
+//         name: rName.value,
+//         email: rEmail.value,
+//         password: rPassword.value
+//     }
+//     usersData.push(userObj)
+
+
+//     rName.value = ""
+//     rEmail.value = ""
+//     rPassword.value = ""
+
+//     console.log(usersData);
+//     localStorage.setItem("users", JSON.stringify(usersData))
+//     var fetchingData = JSON.parse(localStorage.getItem("users"));
+//     fetchingData.push(userObj)
+
+
+//     // location.href = "login.html"
+
+
+// })
+
+
+// var loginBtn = document.getElementById("login")
+
+
+// loginBtn && loginBtn.addEventListener("click", function (event) {
+
+
+//     var loginEmail = document.getElementById("loginEmail")
+//     var loginPass = document.getElementById("loginPassword")
+
+//     var users = JSON.parse(localStorage.getItem("users"));
+
+
+//     for (var user of users) {
+//         if (user.email == loginEmail.value) {
+
+//             if (user.password == loginPass.value) {
+//                 console.log("login successfully");
+
+//             }
+//             else {
+//                 console.log("email is ok wrong password");
+
+//             }
+
+//         }
+//         else {
+//           if(loginPass.value == user.password){
+//             console.log("password is ok email is wrong");
+            
+//           }
+
+//         }
+
+//     }
+// })
+
+
+
+var id = setInterval(function(){
+    console.log("hello");
     
+},1000)
 
-    window.location.href = "login.html"
+console.log(id);
+
+
+setTimeout(function(){
+   clearInterval(id)
     
-    
-})
-
-
-var loginBtn = document.getElementById("login")
-
-
-loginBtn.addEventListener("click",function(event){
-
-    event.preventDefault()
-    var loginEmail = document.getElementById("registerEmail")
-    var loginPass = document.getElementById("registerPassword")
-
-    console.log(loginEmail.value,loginPass.value);
-    
-})
+},5000)
